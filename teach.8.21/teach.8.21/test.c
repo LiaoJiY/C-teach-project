23d30b7403eb98b6578d20caa2e3edb42ebb0e22
LiaoJiY <3245959653@qq.com>
2024-08-22 16:09:49 +0800

    2024 8 22 代码提交

---
+++ teach.8.21/teach.8.21/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//
+//	return 0;
+//}
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%c,b=%c,c=%c", a, b, c);
+//	return 0;
+//}
+//int i = -20;
+//unsigned int j = 10;
+//printf("%d\n", i + j);
